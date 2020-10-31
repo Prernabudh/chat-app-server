b23fbbac209f831bdf02b668cb4bf9c33dc9aa2f
Prerna Budhraja <prernabudhraja8@gmail.com>
2020-10-31 10:51:56 +0530

    after_status

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/user");
 
 router.post("/login", userController.login);
 router.post("/register", userController.register);
+router.post("/getUser", userController.getUser);
 
 module.exports = router;
